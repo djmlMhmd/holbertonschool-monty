a69c38b030a548ee464c5d535c5be0ee8ba11f51
djamal <djamaldine-mohamed@live.fr>
2023-05-11 07:09:33 -0700

    split the file main.c

---
+++ parse_line.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * parse_line - Parses a line of input from a Monty bytecode file and processes
+ * it according to the corresponding opcode.
+ * @line: The line of input to parse.
+ * @line_len: The length of the line of input.
+ * @line_number: The line number of the input.
+ * @stack: A pointer to the top of the stack.
+ */
+void parse_line(char *line,
+		size_t line_len __attribute__((unused)),
+		unsigned int line_number,
+		stack_node_t **stack)
+{
+		char *opcode = strtok(line, " \n");
+
+		if (!opcode || opcode[0] == '#')
+		return;
+
+		if (strcmp(opcode, "push") == 0)
+			push(stack, line_number);
+
+		else if (strcmp(opcode, "pall") == 0)
+			pall(stack, line_number);
+
+		else if (strcmp(opcode, "pint") == 0)
+			pint(stack, line_number);
+		else
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+		exit(EXIT_FAILURE);
+	}
+}
---
+++ process_file.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * process_file - Processes a Monty bytecode file line by line.
+ * @file: A pointer to the Monty bytecode file.
+ * @stack: A pointer to the top of the stack.
+ */
+void process_file(FILE *file, stack_node_t **stack)
+{
+	char *line = NULL;
+	size_t line_len = 0;
+	unsigned int line_number = 0;
+	ssize_t read;
+
+		while ((read = getline(&line, &line_len, file)) != -1)
+	{
+		line_number++;
+		parse_line(line, line_len, line_number, stack);
+	}
+
+		free(line);
+}
